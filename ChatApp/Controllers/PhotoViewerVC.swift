2f63450a3f2415e8c369a599fbb172be183c337b
Ahmed Ezzat <ahmedezzat@Ahmeds-MacBook-Pro.local>
2020-08-28 14:46:26 +0200

    getting started

---
+++ ChatApp/Controllers/PhotoViewerVC.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoViewerVC.swift
+//  ChatApp
+//
+//  Created by Ahmed Ezzat on 8/28/20.
+//  Copyright © 2020 IDEAcademy. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ChatApp/ConversationsVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Ahmed Ezzat on 8/28/20.
+//  Copyright © 2020 IDEAcademy. All rights reserved.
+//
+
+import UIKit
+
+class ConversationsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
